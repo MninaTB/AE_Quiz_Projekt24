a03d1946876900da52144e1820042012e879c8cd
Nina Olear <pninak@web.de>
2021-04-16 21:55:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# AE_Quiz_Projekt24
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class Main {
+	public static void main(String[] args) {
+		JFrame f = new JFrame("Fenster");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel contentPanel = new JPanel();
+		contentPanel.setLayout(new FlowLayout());
+		
+		f.setTitle("Ein GUI");
+		f.setSize(250, 250);
+		f.setVisible(true);
+		
+		f.getContentPane().add(BorderLayout.CENTER, contentPanel);
+	}
+}
