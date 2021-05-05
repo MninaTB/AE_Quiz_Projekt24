11977e750d684ef3faa414aab1c5f43bf3b2fe1b
NiklasNeumann <niklasneumann48@gmail.com>
2021-05-05 12:55:26 +0200

    Änderungen an den Tabellen vorgenommen damit diese zusammen funktionieren

--- scripts/SQLBefehle.sql
+++ scripts/SQLBefehle.sql
@@ -1,22 +1,34 @@
 -- Unser Name der Tabelle ist "Fragen" - "Question"
 
 -- The DROP TABLE statement is used to drop an existing table in a database.
-DROP TABLE IF EXISTS Question;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS categories;
 
--- Creates new Table
-CREATE TABLE Question(
-id INT NOT NULL,
-question VARCHAR(255) NOT NULL,
-difficulty INT NOT NULL,
-answers varchar(255) NOT NULL,
-FOREIGN KEY (answers) REFERENCES Answers(answers_answerTable),
-solution INT NOT NULL,
-category VARCHAR NOT NULL,
-PRIMARY KEY(id)
-)
+CREATE TABLE categories(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255),
+  PRIMARY KEY(id)
+);
 
+CREATE TABLE IF NOT EXISTS answers (
+  id INT UNSIGNED AUTO_INCREMENT,
+  a VARCHAR(255) NOT NULL,
+  b VARCHAR(255) NOT NULL,
+  c VARCHAR(255) NOT NULL,
+  d VARCHAR(255) NOT NULL,
+  PRIMARY KEY(id)
+);
 
--- Creates new Table with Answers
-CREATE TABLE Answers(
-answers_answerTable varchar(255) NOT NULL,
-)
+CREATE TABLE IF NOT EXISTS questions (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  question VARCHAR(255) NOT NULL,
+  difficulty INT NOT NULL,
+  answer_id INT UNSIGNED NOT NULL,
+  solution INT NOT NULL,
+  category_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY (answer_id) REFERENCES answers(id) ON DELETE CASCADE,
+  FOREIGN KEY (category_id) REFERENCES categories(id),
+  PRIMARY KEY (id)
+);
+
