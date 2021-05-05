bd97bd777c78acd96cb67fd34227a38cd198a965
NiklasNeumann <niklasneumann48@gmail.com>
2021-05-05 08:44:09 +0200

    Habe die unnützigen Befehle entfernt und außerdem die Attribute unabhängig von den SQL-Befehlen klein geschrieben.

--- scripts/SQLBefehle.sql
+++ scripts/SQLBefehle.sql
@@ -1,32 +1,16 @@
----------------------------
--- Created by Niklas N.
----------------------------
-
--- Unser Name der Tabelle ist "Fragen"
+-- Unser Name der Tabelle ist "Fragen" - "Question"
 
 -- The DROP TABLE statement is used to drop an existing table in a database.
-DROP TABLE IF EXISTS Fragen;
+DROP TABLE IF EXISTS Question;
 
 -- Creates new Table
-CREATE TABLE Fragen(
-ID INT NOT NULL,
-QUESTIONS VARCHAR(5) NOT NULL,
-DIFFICULTY INT NOT NULL,
-ANSWERS VARCHAR(20),
-SOLUTION INT,
-CATEGORY VARCHAR,
+CREATE TABLE Question(
+id INT NOT NULL,
+question VARCHAR(255) NOT NULL,
+difficulty INT NOT NULL,
+answers varchar(255) NOT NULL,
+FOREIGN KEY (answers) REFERENCES Answers(answers_other_table),
+solution INT NOT NULL,
+category VARCHAR NOT NULL,
 PRIMARY KEY(id)
 )
-
--- Insert Values into Database
-INSERT INTO Fragen (ID, QUESTIONS, DIFFICULTY, ANSWERS, SOLUTION, CATEGORY)
-VALUES ("ID", "QUESTIONS", "DIFFICULTY", "ANSWERS", "SOLUTION", "CATEGORY");
-
--- The ALTER TABLE statement is used to add, delete, or modify columns in an existing table.
--- Example
-ALTER TABLE Fragen
-ADD MailFromUser VARCHAR(255);
-
--- The SELECT statement is used to select data from a database.
--- Example
-SELECT ID, QUESTIONS FROM Fragen;
