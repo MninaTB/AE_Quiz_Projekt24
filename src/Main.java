e64f6f20e0a10de90e5ade8183744063815cd262
Nina Olear <pninak@web.de>
2021-06-07 18:29:05 +0200

    feat(src/Main): unterstutze Datenbank verbindung ueber args

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 import controller.Screen;
+import questions.QuestionDB;
 import questions.QuestionParser;
+import questions.QuestionStore;
 import ui.Registry;
 import ui.Router;
 
@@ -22,21 +27,56 @@ import ui.Router;
  */
 public class Main {
 	public static void main(String[] args) {
-		String filename = "questions.json";
-		QuestionParser q = new QuestionParser();
-		File file = new File(filename);
-		try {
-			InputStreamReader sr = new InputStreamReader(new FileInputStream(file));
-			q.load(sr);
-		} catch (FileNotFoundException e) {
-			System.out.printf("could not load: %s\n", filename);
+		final String fallback = "questions.json";
+		String filename = "";
+		File file = null;
+		QuestionParser q = null;
+		QuestionStore store = null;
+		if (args.length > 0) {
+			if (args[0].contains("sql")) {
+				if (args.length < 2) {
+					System.out.println("missing arguments (url, username, password) want: 3 got: " + args.length);
+				}
+				Connection connection = null;
+				String url = args[0];
+				String username = args[1];
+				String password = args[2];
+				try {
+					connection = DriverManager.getConnection(url, username, password);
+				} catch (SQLException e) {
+					System.out.println("An error occurred while connecting MySQL databse");
+					e.printStackTrace();
+				}
+				store = new QuestionDB(connection);
+			} else if (args[0].contains("json")) {
+				filename = args[0];
+			} else {
+				System.out.println("invalid argument: " + args[0]);
+				System.exit(1);
+			}
+		} else {
+			System.out.println("missing connection arguments, use fallback store: " + fallback);
+			filename = fallback;
 		}
+		
+		if (store == null) {
+			q = new QuestionParser();
+			file = new File(filename);
+			try {
+				InputStreamReader sr = new InputStreamReader(new FileInputStream(file));
+				q.load(sr);
+			} catch (FileNotFoundException e) {
+				System.out.printf("could not load: %s\n", filename);
+			}
+			store = q;
+		}
+
 
 		JFrame f = new JFrame();
 
 		Registry reg = new Registry();
 		Router router = new Router(f, reg, Screen.SCREEN_START);
-		reg.init(router.getSwitcher(), q);
+		reg.init(router.getSwitcher(), store);
 
 		Runnable spawner = new Runnable() {
 			public void run() {
@@ -56,17 +96,19 @@ public class Main {
 		SwingUtilities.invokeLater(spawner);
 		System.out.println("Run router!");
 		router.Run();
-		try {
-			OutputStream os = new FileOutputStream(file);
-			OutputStreamWriter osw = new OutputStreamWriter(os);
-			q.save(osw);
-			osw.flush();
-		} catch (Exception e) {
-			System.out.print("Failed to save questions, file: ");
-			System.out.println(filename);
-			System.exit(1);
+		if (file != null) {
+			try {
+				OutputStream os = new FileOutputStream(file);
+				OutputStreamWriter osw = new OutputStreamWriter(os);
+				q.save(osw);
+				osw.flush();
+			} catch (Exception e) {
+				System.out.print("Failed to save questions, file: ");
+				System.out.println(filename);
+				System.exit(1);
+			}
+			System.out.println("Saved questions!");
 		}
-		System.out.println("Saved questions!");
 		System.out.println("Bye!");
 		System.exit(0);
 	}
